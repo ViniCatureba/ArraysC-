7602158a1b794ad25b32a177fffd811c8f9e7811
Vinicius Catureba <vinicius.bigao14@gmail.com>
2025-03-20 21:20:23 -0300

    Exercicios array

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//listagem de elementos
+
+int[] arryadoscria = [1, 2, 3, 4, 5];
+
+for (int i= 0; i < arryadoscria.Length; i++)
+{
+    Console.WriteLine($"Index {i} valor "+arryadoscria[i]);
+}
+
+
+
+//soma dos elementos de um array
+
+int[] somadoe5 = [1,2,3,4,5];
+int somatotal = 0;
+for (int i = 0; i < somadoe5.Length; i++)
+{
+    somatotal += somadoe5[i];
+}
+
+Console.WriteLine("soma total dos 5"+somatotal);
+
+
+
+//Encontrar o maior numero do arrary
+
+int[] maiorarray = [0, 7, 131, 2, 30];
+int maior = 0;
+for(int i = 0; i < maiorarray.Length;i++)
+{
+    if (maiorarray[i] > maior)
+    {
+        maior = maiorarray[i];
+    }
+}
+
+Console.WriteLine($"O maior numero da array é {maior}");
+
+
+//Contagem de numeros pares
+int[] listadenumeros = new int[10];
+int varaux = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Insira o {i +1}º numero: ");
+    int inputnum = int.Parse(Console.ReadLine());
+    if (inputnum%2 == 0)
+    {
+        listadenumeros[varaux] = inputnum;
+        varaux++;
+    }
+}
+
+int tamanho = 0;
+for(int i = 0; i < listadenumeros.Length; i++)
+{
+    if (listadenumeros[i] != 0)
+    {
+        tamanho++;
+    }
+}
+Console.WriteLine($"O Array possui: {tamanho} numeros pares");
+
+//Inversao de elementos
+
+
+
+// Projeto de revisao
+
--- ConsoleApp1/obj/Debug/net8.0/Ex.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/Ex.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d00bdc967468cbf6ad5fd3921b8d7eaabe3cbd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
